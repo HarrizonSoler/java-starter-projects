5d3690e2ff8dbea84475da5576cfd26f53ee90f5
Harrizon Alexander Soler Galindo <harrizon.soler@usantoto.edu.co>
2020-04-16 11:48:42 -0500

    Añadido Actividad 3
    Añadido Comentario Inicial y Reformat Code

--- P2T3_Functions/src/usta/sistemas/Main.java
+++ P2T3_Functions/src/usta/sistemas/Main.java
@@ -3,11 +3,17 @@ package usta.sistemas;
 import java.util.Scanner;
 
 public class Main {
+       /*
+      Name: Harrizon Alexander Soler Galindo
+      Date: 15/04/2020
+      Description: This software calculates the area of 3 geometric figures).
+    */
 
     public static void main(String[] args) {
-	menu();
+        menu();
     }
-    public static void menu(){
+
+    public static void menu() {
         Scanner keyboard = new Scanner(System.in);
         byte op;
 
@@ -21,15 +27,19 @@ public class Main {
 
         f_error_op(op);
 
-        switch (op){
-            case 1: a_circle();
-            break;
-            case 2: a_triangle();
-            break;
-            case 3: a_square();
+        switch (op) {
+            case 1:
+                a_circle();
+                break;
+            case 2:
+                a_triangle();
+                break;
+            case 3:
+                a_square();
         }
     }
-    public static void a_circle(){
+
+    public static void a_circle() {
         double rad;
         Scanner keyboard = new Scanner(System.in);
 
@@ -38,10 +48,11 @@ public class Main {
 
         rad = f_error(rad);
 
-        System.out.println("The area of the circle is " + (Math.pow(rad,2) * Math.PI) + " cm2");
+        System.out.println("The area of the circle is " + (Math.pow(rad, 2) * Math.PI) + " cm2");
     }
-    public static void a_triangle(){
-        double base,height;
+
+    public static void a_triangle() {
+        double base, height;
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Type the base of the triangle (cm)");
@@ -54,9 +65,10 @@ public class Main {
 
         height = f_error(height);
 
-        System.out.println("The area of the triangle is "+ ((base*height)/2) + " cm2");
+        System.out.println("The area of the triangle is " + ((base * height) / 2) + " cm2");
     }
-    public static void a_square(){
+
+    public static void a_square() {
         double side;
         Scanner keyboard = new Scanner(System.in);
 
@@ -65,20 +77,22 @@ public class Main {
 
         side = f_error(side);
 
-        System.out.println("The area of the square is " + (Math.pow(side,2) + " cm2"));
+        System.out.println("The area of the square is " + (Math.pow(side, 2) + " cm2"));
     }
-    public static double f_error(double r){
+
+    public static double f_error(double r) {
         Scanner keyboard = new Scanner(System.in);
-                while (r<=0){
-                    System.err.println("ERROR, The input must be greater than 0\nRetry:");
-                    r = keyboard.nextDouble();
-                }
-                return r;
+        while (r <= 0) {
+            System.err.println("ERROR, The input must be greater than 0\nRetry:");
+            r = keyboard.nextDouble();
         }
-    public static byte f_error_op(byte o){
+        return r;
+    }
+
+    public static byte f_error_op(byte o) {
         Scanner keyboard = new Scanner(System.in);
 
-        while (o<1 || o>3){
+        while (o < 1 || o > 3) {
             System.err.println("ERROR, The input must be between 1 and 3\nRetry:");
             o = keyboard.nextByte();
         }
