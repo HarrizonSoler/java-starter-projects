01e636f7d5b3cd68ebd89b76175c6d25459395eb
Harrizon Alexander Soler Galindo <harrizon.soler@usantoto.edu.co>
2020-04-29 01:28:49 -0500

    Actividad 14

---
+++ P2T14_N/src/usta/sistemas/Main.java
@@ -0,0 +1,57 @@
+package usta.sistemas;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int stops = f_stops(),passengers[] ,pin = 0,pout = 0;
+
+        for (int i = 1; i <= stops; i++){
+            passengers = f_passengers(i);
+
+            pin += passengers[0];
+            pout += passengers[1];
+        }
+
+        System.out.println("A total of " + pin + " passengers entered and " + pout + " passengers left the Bus with " + stops + " stops");
+        System.out.println(pin*150 + " dollars were obtained");
+
+    }
+    public static int f_stops(){
+        Scanner keyboard = new Scanner(System.in);
+        int stops;
+
+        System.out.println("Type the number of stops in the route:");
+        stops = f_input();
+
+        return stops;
+    }
+    public static int[] f_passengers(int i){
+        Scanner keyboard = new Scanner(System.in);
+        int passengers[] = new int[2];
+
+        System.out.println("Type the number of get in passengers in the stop " + i);
+        passengers[0] = f_input();
+
+        if (i != 1){
+            System.out.println("Type the number of get off passengers in the stop " + i);
+            passengers[1] = f_input();
+        }
+
+        return passengers;
+    }
+    public static int f_input(){
+        Scanner keyboard = new Scanner(System.in);
+        int input;
+
+        input = keyboard.nextInt();
+
+        while (input < 1){
+            System.err.println("ERROR, The input must be greater than 0\nRetry:");
+            input = keyboard.nextInt();
+        }
+
+        return input;
+    }
+}
