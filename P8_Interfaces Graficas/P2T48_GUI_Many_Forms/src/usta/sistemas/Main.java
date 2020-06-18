f6b42289982df81faf969d642791035828a43117
Harrizon Alexander Soler Galindo <harrizon.soler@usantoto.edu.co>
2020-06-18 11:20:21 -0500

    Proyecto

--- P8_Interfaces Graficas/P2T48_GUI_Many_Forms/src/usta/sistemas/Main.java
+++ P8_Interfaces Graficas/P2T48_GUI_Many_Forms/src/usta/sistemas/Main.java
@@ -5,7 +5,13 @@ import java.awt.*;
 
 public class Main {
 
+    /*
+      Name: Harrizon Alexander Soler Galindo
+      Date: 17/06/2020
+      Description: This class call the FormStart Window;
+    */
+
     public static void main(String[] args) {
-	FormStart start = new FormStart();
+	FormStart start = new FormStart(); //Call the new Window
     }
 }
